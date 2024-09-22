21d06245802c440e1ae9cdf5fe773cbac6015b5c
NastyaParnet <nastya.parnet.01@gmail.com>
2024-09-22 14:15:39 +0300

    ready task

--- statistics.js
+++ statistics.js
@@ -1,5 +1,14 @@
 const { groupBy, mergeMap, reduce, map } = require("rxjs");
 
 module.exports = function getGroupStatistics(students$) {
-  // Your code here
+  return students$
+    .pipe(
+      groupBy(p => p.group),
+      mergeMap(group$ => group$.pipe(
+        reduce((acc, cur) => (
+          {group: cur.group, sumRatings: acc.sumRatings + cur.rating, count: acc.count + 1}
+        ), {group: '', sumRatings: 0, count: 0})
+      )),
+      map(({group, sumRatings, count}) => ({group, avgRating: sumRatings / count }))
+    )
 };
